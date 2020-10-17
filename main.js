0819cb6682b7f61b2de972c3c96317d7db795e90
adibafatin <51741607+adibafatin@users.noreply.github.com>
2020-10-17 12:51:59 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+
+function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+function pageTransition() {
+    var tl = gsap.timeline();
+    tl.to(".loading-screen", {
+        duration: 1.2,
+        width: "100%",
+        top: "0%",
+        ease: "Expo.easeInOut",
+    });
+
+    tl.to(".loading-screen", {
+        duration: 1,
+        width: "100%",
+        top: "100%",
+        ease: "Expo.easeInOut",
+        delay: 0.3,
+    });
+    tl.set(".loading-screen", { top: "100%" });
+}
+
+function contentAnimation() {
+    var tl = gsap.timeline();
+    tl.from(".animate-this", { duration: 1, y: -80, opacity: 0, stagger: 0.4, delay: 0.2 });
+}
+
+$(function () {
+    barba.init({
+        sync: true,
+
+        transitions: [
+            {
+                async leave(data) {
+                    const done = this.async();
+
+                    pageTransition();
+                    await delay(1000);
+                    done();
+                },
+
+                async enter(data) {
+                    contentAnimation();
+                },
+
+                async once(data) {
+                    contentAnimation();
+                },
+            },
+        ],
+    });
+});
